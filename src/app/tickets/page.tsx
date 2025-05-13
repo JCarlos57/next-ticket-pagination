3db96de95e33c16013577fe5a0f257b7185dd04c
Juan Carlos <jcroyo@gmail.com>
2025-05-13 09:45:37 -0400

    shadcn installed

---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,11 @@
+export default function TicketsPage() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl font-bold bg-sky-600 text-white py-2 px-3 text-center">
+          Tailwind Installed !!!
+        </h1>
+      </div>
+    </main>
+  );
+}
